31161728dded1a8a04ea542dafd1a45c3bae10f9
Cameron Webb <cwebb@Camerons-MacBook-Pro.local>
2013-11-12 16:10:18 -0500

    Adding in a bunch files

---
+++ iOSSDK/iOSSDK/iOSSDK.h
@@ -0,0 +1,13 @@
+//
+//  iOSSDK.h
+//  iOSSDK
+//
+//  Created by Cameron Webb on 11/12/13.
+//  Copyright (c) 2013 Sqwiggle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSSDK : NSObject
+
+@end
---
+++ iOSSDK/iOSSDK/iOSSDK.m
@@ -0,0 +1,13 @@
+//
+//  iOSSDK.m
+//  iOSSDK
+//
+//  Created by Cameron Webb on 11/12/13.
+//  Copyright (c) 2013 Sqwiggle. All rights reserved.
+//
+
+#import "iOSSDK.h"
+
+@implementation iOSSDK
+
+@end
