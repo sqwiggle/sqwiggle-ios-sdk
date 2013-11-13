a461717776513278f001c2956a6ad5033252eef0
Cameron Webb <cwebb@Camerons-MacBook-Pro.local>
2013-11-13 15:48:07 -0500

    Invite model

---
+++ iOSSDK/Invite.h
@@ -0,0 +1,21 @@
+//
+//  Invite.h
+//  SqwiggleSDK
+//
+//  Created by Cameron Webb on 11/13/13.
+//  Copyright (c) 2013 Sqwiggle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "User.h"
+#import "Company.h"
+
+@interface Invite : NSObject
+
+@property (nonatomic, readonly) int id;
+@property (nonatomic, readonly) User *from;
+@property (nonatomic, readonly) Company *company;
+@property (nonatomic, readonly) NSURL *avatar;
+@property (nonatomic, readonly) NSURL *url;
+
+@end
---
+++ iOSSDK/Invite.m
@@ -0,0 +1,13 @@
+//
+//  Invite.m
+//  SqwiggleSDK
+//
+//  Created by Cameron Webb on 11/13/13.
+//  Copyright (c) 2013 Sqwiggle. All rights reserved.
+//
+
+#import "Invite.h"
+
+@implementation Invite
+
+@end
