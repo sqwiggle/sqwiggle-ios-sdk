e544678fca96db5e40248f06aafc886f748f725a
Cameron Webb <cwebb@Camerons-MacBook-Pro.local>
2013-11-13 10:02:04 -0500

    reorganizing a few things

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '6.0'
+
+pod 'AFNetworking', '~> 2.0.2'
+pod 'ObjectiveSugar', '~> 0.9'
+
+target 'Unit Tests' do
+    pod 'OHHTTPStubs', '~> 3.0.2'
+end
---
+++ iOSSDK/Sqwiggle.m
@@ -0,0 +1,13 @@
+//
+//  Sqwiggle.m
+//  libSqwiggle
+//
+//  Created by Cameron Webb on 11/12/13.
+//  Copyright (c) 2013 Sqwiggle. All rights reserved.
+//
+
+#import "Sqwiggle.h"
+
+@implementation Sqwiggle
+
+@end
