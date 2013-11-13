9626471955f00d41f68c3a1491aece1227def441
Cameron Webb <cwebb@Camerons-MacBook-Pro.local>
2013-11-13 16:26:58 -0500

    Adding notification model

---
+++ iOSSDK/Notification.h
@@ -0,0 +1,13 @@
+//
+//  Notification.h
+//  SqwiggleSDK
+//
+//  Created by Cameron Webb on 11/13/13.
+//  Copyright (c) 2013 Sqwiggle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Notification : NSObject
+
+@end
---
+++ iOSSDK/Notification.m
@@ -0,0 +1,13 @@
+//
+//  Notification.m
+//  SqwiggleSDK
+//
+//  Created by Cameron Webb on 11/13/13.
+//  Copyright (c) 2013 Sqwiggle. All rights reserved.
+//
+
+#import "Notification.h"
+
+@implementation Notification
+
+@end
