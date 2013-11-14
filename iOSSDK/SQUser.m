98a2d0d0657ebbbdb53777e070d15ed1afbf0949
Cameron Webb <cwebb@Camerons-MacBook-Pro.local>
2013-11-14 18:14:01 -0500

    removed comment properties in squser

--- iOSSDK/SQUser.m
+++ iOSSDK/SQUser.m
@@ -21,19 +21,6 @@
 @implementation SQUser
 
 
-/*@property (nonatomic, readonly) NSString *role;
- @property (nonatomic, readonly) enum MediaMode *mode;
- @property (nonatomic, readonly) NSString *status;
- @property (nonatomic, readonly) NSString *name;
- @property (nonatomic, readonly) NSString *email;
- @property (nonatomic, readonly) BOOL confirmed;
- @property (nonatomic, readonly) NSTimeZone *timeZone;
- @property (nonatomic, readonly) float timeZoneOffset;
- @property (nonatomic, readonly) NSDate *createdAt;
- @property (nonatomic, readonly) NSDate *lastActiveAt;
- @property (nonatomic, readonly) NSURL *avatar;*/
-
-
 -(NSDictionary *) modelDefinition
 {
     return @{_role : @"role", _mode: @"mode",
