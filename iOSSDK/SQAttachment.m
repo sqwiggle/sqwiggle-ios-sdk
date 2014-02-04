74d01df78d93704195b817ef0d6501b45e20407c
Cameron Webb <cwebb02@gmail.com>
2014-02-04 18:13:31 -0500

    removing unused variables

--- iOSSDK/SQAttachment.m
+++ iOSSDK/SQAttachment.m
@@ -10,15 +10,11 @@
 
 @interface SQAttachment ()
 {
-    NSNumber *_animated;
-    NSString *_type;
     NSString *_url;
     NSString *_title;
     NSString *_description;
     NSString *_image;
     NSString *_status;
-    NSDate *_createdAt;
-    NSDate *_updatedAt;
 }
 @end
 
@@ -35,11 +31,6 @@
     return @{@"_ID": @"id", @"_url" : @"url", @"_title": @"title",
              @"_description": @"description",
              @"_image": @"image", @"_status": @"status"};
-}
-
--(BOOL) animated
-{
-    return [_animated boolValue];
 }
 
 @end
