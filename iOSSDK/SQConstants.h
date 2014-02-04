b30ae21a131cfdee0fca53f85be88aeaa65f9e91
Cameron Webb <cwebb02@gmail.com>
2014-02-04 18:44:06 -0500

    Straightening out a few things in the SDK

--- iOSSDK/SQConstants.h
+++ iOSSDK/SQConstants.h
@@ -9,16 +9,10 @@
 #define SQWIGGLE_SDK_VERION @"0.0.1"
 
 #define ENVIRONMENT_PRODUCTION @"production"
-#define ENVIRONMENT_STAGING @"staging"
 
 #define APP_ENVIRONMENT ENVIRONMENT_PRODUCTION
 
-#define PUSHER_ENVIRONMENT_TOKENS @{ENVIRONMENT_PRODUCTION: @"fd4dd9eb82163e6920a0", \
-ENVIRONMENT_STAGING: @"ab85ca06db9fadbf7ee3"}
-
-#define PUSHER_API_TOKEN PUSHER_ENVIRONMENT_TOKENS[APP_ENVIRONMENT]
-
-#define SQWIGGLE_ENDPOINTS @{ENVIRONMENT_PRODUCTION: @"https://api.sqwiggle.com", ENVIRONMENT_STAGING: @"https://sqwiggle-staging-api.herokuapp.com"}
+#define SQWIGGLE_ENDPOINTS @{ENVIRONMENT_PRODUCTION: @"https://api.sqwiggle.com"}
 
 #define SQWIGGLE_URI_API SQWIGGLE_ENDPOINTS[APP_ENVIRONMENT]
 
@@ -42,7 +36,9 @@ ENVIRONMENT_STAGING: @"ab85ca06db9fadbf7ee3"}
                                 SQWIGGLE_CONVERSATION_TYPE: @"conversations",\
                                 SQWIGGLE_ROOM_TYPE: @"rooms",\
                                 SQWIGGLE_INVITE_TYPE: @"invites", \
-                                SQWIGGLE_MESSAGE_TYPE: @"messages" }
+                                SQWIGGLE_MESSAGE_TYPE: @"messages", \
+                                SQWIGGLE_CONFIGURATION_TYPE: @"info/configuration", \
+                                SQWIGGLE_ATTACHMENT_TYPE: @"attachments"}
 
 #import <ObjectiveSugar/ObjectiveSugar.h>
 #import <AFNetworking/AFNetworking.h>
