1f5876a1603618be68734317aa188e79bd0cfc59
Cameron Webb <cwebb02@gmail.com>
2014-09-04 14:02:50 -0500

    changing colors and alpha settings

--- iOSSDK/SQConversation.h
+++ iOSSDK/SQConversation.h
@@ -10,11 +10,11 @@
 #import "SQRoom.h"
 #import "SQOrganization.h"
 
-#define COLOR_ONE [UIColor colorWithRed:0.0/255 green:147.0/255 blue:255.0/255.0 alpha:0.65]
-#define COLOR_TWO [UIColor colorWithRed:224.0/255 green:78.0/255 blue:97.0/255.0 alpha:0.65]
-#define COLOR_THREE [UIColor colorWithRed:224.0/255 green:188.0/255 blue:78.0/255.0 alpha:0.65]
-#define COLOR_FOUR [UIColor colorWithRed:0.0/255 green:204.0/255 blue:0.0/255.0 alpha:0.65]
-#define COLOR_FIVE [UIColor colorWithRed:159.0/255 green:89.0/255 blue:160.0/255.0 alpha:0.65]
+#define COLOR_ONE [UIColor colorWithRed:0.0/255 green:64.0/255 blue:134.0/255.0 alpha:1]
+#define COLOR_TWO [UIColor colorWithRed:221.0/255 green:48.0/255 blue:70.0/255.0 alpha:1]
+#define COLOR_THREE [UIColor colorWithRed:204.0/255 green:162.0/255 blue:34.0/255.0 alpha:1]
+#define COLOR_FOUR [UIColor colorWithRed:43.0/255 green:172.0/255 blue:0.0/43.0 alpha:1]
+#define COLOR_FIVE [UIColor colorWithRed:155.0/255 green:60.0/255 blue:156.0/255.0 alpha:1]
 
 #define COLOR_DEFS @{ @1: COLOR_ONE, @2: COLOR_TWO, @3: COLOR_THREE, @4: COLOR_FOUR, @5: COLOR_FIVE}
 
